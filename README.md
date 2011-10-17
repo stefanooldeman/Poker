473c14d5202f21484d7243b86666a5a660391574
stefanooldeman <stefano.oldeman@gmail.com>
2011-10-17 23:57:05 +0200

    add shuffleDeck using monads

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ### A Poker game build in Haskell
 
-note : Some decicions are function results are influenced by a given assignement.
+note : Some decicions and function names are heavily influenced by a given assignement:
+
+
 the assignement can be found in [pdf here]( http://www.cs.uu.nl/wiki/pub/FP/PracticalExercises/Poker.pdf)
+
 
 EOF
--- deck.hs
+++ deck.hs
@@ -1,4 +1,8 @@
 module Deck where 
+
+import System.Random
+import Data.Map
+
 import Cards
 
 type Deck = [Card]
@@ -7,4 +11,14 @@ type Hand = [Card]
 fullDeck :: Deck
 fullDeck = [Card v s | v <-[Two .. Ace], s <-[Spades,Hearts,Diamonds,Clubs]]
 
+shuffleDeck :: Deck -> IO Deck
+shuffleDeck xs = shuffle' xs (length xs)
+    where shuffle' _ 0    = return []
+          shuffle' xs len = do n           <- randomRIO (0, len - 1)
+                               let (y, ys) =  choose n xs
+                               ys'         <- shuffle' ys (len - 1)
+                               return (y:ys')
 
+choose _ []     = error "choose: index out of range"
+choose 0 (x:xs) = (x, xs)
+choose i (x:xs) = let (y, ys) = choose (i - 1) xs in (y, x:ys)
