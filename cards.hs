ee48b4d1a93621eb7982fb5b7223279d743ad736
stefanooldeman <stefano.oldeman@gmail.com>
2011-10-17 22:34:37 +0200

    the data types in module Cards

---
+++ cards.hs
@@ -0,0 +1,36 @@
+-- Card module
+
+data Suit = Spades | Hearts | Diamonds | Clubs
+    deriving (Eq, Enum)
+
+instance Show Suit where
+    show Spades = "S"
+    show Hearts = "H"
+    show Diamonds = "D"
+    show Clubs = "C"
+
+data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+    deriving (Eq, Ord, Enum)
+
+instance Show Value where 
+    show Two = "2"
+    show Three = "3"
+    show Four = "4"
+    show Five = "5"
+    show Six = "6"
+    show Seven = "7"
+    show Eight = "8"
+    show Nine = "9"
+    show Ten = "10"
+    show Jack = "J"
+    show Queen = "Q"
+    show King = "K"
+    show Ace = "A"
+
+data Card = Card {value :: Value, suit :: Suit}
+    deriving (Eq)
+
+instance Show Card where
+    show (Card value suit) = show value ++ show suit
+
+-- next module Deck
