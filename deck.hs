5b61ea2ad295d3bd06081dc25b1c102a2f2e5a41
stefanooldeman <stefano.oldeman@gmail.com>
2012-03-31 11:49:56 +0200

    deal cards, draft

--- deck.hs
+++ deck.hs
@@ -29,3 +29,15 @@ shuffleDeck xs = parse_deck $ shuffle' xs (length xs)
         parse_deck :: IO Deck -> Deck
         parse_deck io = unsafePerformIO io
 
+--
+-- dealNCards :: Int -> Deck -> ([Card], Deck)
+dealNCards n xxs | n < 1 = error "the minimum required for N is 1"
+                 | otherwise = f xxs
+    where
+        f :: [a] -> ([a], [a])
+        f (x:xs) = f1 ([], x:xs)
+        f1 :: ([a], [a]) -> ([a], [a])
+        f1 (t0, y:yx) -- = (y:t0, yx)
+            | n >= length t0 = f1 (y:t0, yx)
+            | otherwise      = (t0, y:yx)
+
