f719cbc3cbccae696aa5345bdd784af13593b494
stefanooldeman <stefano.oldeman@gmail.com>
2012-04-21 22:05:36 +0200

    finish isOnePair with refactoring

--- hands.hs
+++ hands.hs
@@ -9,15 +9,14 @@ import Cards
 
 type Hand = [Card]
 data Combo = StraightFlush | FourOfAKind | FullHouse | Flush | 
-             Straight | ThreeOfAKind | OnePair | HighCards
+             Straight | ThreeOfAKind | OnePair Card Card | HighCards
     deriving (Show, Ord, Eq)
 
 isOnePair :: Hand -> Maybe Combo 
-isOnePair (Card value1 suit1 :Card value2 suit2 :xs) = case value1 == value2 of
-                                                           True  -> Just OnePair
-                                                           False -> isOnePair (Card {value = value2, suit = suit2}:xs)
+isOnePair (card1@(Card value1 _) :card2@(Card value2 _) :xs) = case value1 == value2 of
+                                                           True  -> Just (OnePair card1 card2)
+                                                           False -> isOnePair (card2:xs)
 isOnePair (_:xs) = Nothing
-
 
 -- quickcheck... isOnePair [Card {value = Two, suit = Spades }, Card {value = Two, suit = Hearts}]
 -- [9C,5H,9S,KS,KD]
